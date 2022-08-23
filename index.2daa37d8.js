d75ed1b791bf5116ed7e036b33f00e00528cabac
Andriy362 <100036164+Andriy362@users.noreply.github.com>
2022-08-23 10:46:53 +0000

    Deploying to gh-pages from @ Andriy362/goit-js-hw-11@a0c8117fb0ffc763cfba7f3a7d58475aa3c06eac 🚀

---
+++ index.2daa37d8.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("#search-form"),document.querySelector(".gallery"),document.querySelector(".btn-form");fetch("".concat("https://pixabay.com/api/").concat("29454004-5e5ef32b7401f0e602c44091a","/").concat("?key=${KEY}&image_type=photo&orientation=horizontal&safesearch=true")).then((function(e){return e.json})).then(console.log)}();
+//# sourceMappingURL=index.2daa37d8.js.map
---
+++ index.6bba89d2.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form"),document.querySelector(".gallery"),document.querySelector(".btn-form");fetch("https://pixabay.com/api/29454004-5e5ef32b7401f0e602c44091a/?key=${KEY}&image_type=photo&orientation=horizontal&safesearch=true").then((e=>e.json)).then(console.log);
+//# sourceMappingURL=index.6bba89d2.js.map
